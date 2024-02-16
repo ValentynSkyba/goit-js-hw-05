93fdcbd875d3314a3fbdbabd75973d1575c9e89f
ValentynSkyba <valentynskyba@gmail.com>
2024-02-16 17:19:49 +0000

    update

--- js/task-3.js
+++ js/task-3.js
@@ -13,7 +13,8 @@
 
 
 
-const sortByDescendingFriendCount = users => users.sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = users => users.toSorted((a, b) => b.friends.length - a.friends.length);
+
 
 
 
